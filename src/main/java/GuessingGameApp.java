623d4493f06bef2ff7ecebeae15ec700e5c76b73
Tyler Gandy <tylergandy@ymail.com>
2020-09-10 19:09:50 -0400

    Third Iteration Completed!

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -7,21 +7,22 @@ public class GuessingGameApp
     {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Guessing Game!\nPlease enter a number between 1-10: ");
-        int num = input.nextInt();
+        int count = 1;
+        boolean hasWon = false;
 
-        if(num == 7)
-        {
-            System.out.println("Congratulations! You guessed correctly!");
-        }
-        else if(num == 0)
-        {
-            System.out.println("Please enter a number ONLY between 1-10 and press enter.");
-        }
-        else
-        {
-            System.out.println("Sorry, you guessed wrong!");
-        }
+        do {
+            System.out.print("Guessing Game!\nPlease enter a number between 1-10: ");
+            int num = input.nextInt();
+
+            if (num == 7) {
+                System.out.println("Congratulations! You guessed correctly!");
+                hasWon = true;
+            } else if (num == 0) {
+                System.out.println("Please enter a number ONLY between 1-10 and press enter.");
+            } else {
+                System.out.println("Sorry, you guessed wrong!");
+            }
+        }while(count < 2 && !hasWon);
     }
 
 }
